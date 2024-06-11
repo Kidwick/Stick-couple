a74a136cf986925d8b1d6e5c997a714ae682dc23
Kidwick <115428041+Kidwick@users.noreply.github.com>
2024-06-11 19:51:50 +0300

    Add files via upload

---
+++ Stickcouple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "      O        O        \n";
+    cout << "     /|\\     /|\\      \n";
+    cout << "    / | \\   / | \\     \n";
+    cout<<"     /  |   \\/  |  \\   \n";
+    cout << "      |        |        \n";
+    cout << "     / \\     / \\      \n";
+    cout << "    /   \\   /   \\     \n";
+    cout << "---------------------- \n";
+    cout << "          ______       \n";
+    cout << " ________/      \\_____\n";
+    cout << "|                  o o|\n";
+    cout << "'--O--------------O---'\n";
+    
+    return 0;
+}
